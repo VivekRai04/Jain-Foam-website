64230cae09c39f83f6a4219f91c57487b9e5f804
Vivek Rai <raiv5253@gmail.com>
2026-02-09 22:05:53 +0530

    Removed Unwanted Images and files

--- test-login.ts
+++
@@ -1,9 +0,0 @@
-import bcrypt from 'bcrypt';
-
-const password = 'JainFoam1995';
-const hash = '$2b$10$qKfZ.6Lky.T3YPLysdaK5OTp3h3.bhI8nZFQeICFGnRvz1cBe5tFW';
-
-bcrypt.compare(password, hash).then((result) => {
-  console.log('Password match:', result);
-  process.exit(0);
-});
